420be9c633456072a53a322babb6e8423b4fc7d8
rawalhimal <himalrawal500@gmail.com>
2023-06-27 18:01:32 +0545

    first time commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import TestComponent from './TestComponent';
+
+import { useState } from 'react';
+
 function App() {
+  const [count , setCount] = useState(0)
+  const changeData=()=>{
+    setCount((count) => count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   <TestComponent name="sipalaya infotech"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={changeData}>Clicked Me</button>
+    
     </div>
   );
 }
---
+++ src/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestComponent =(props)=>{
+return(
+    <>
+    <div>
+        <h3> This is test component {props.name} </h3>
+    </div>
+    </>
+)
+}
+
+export default TestComponent;
